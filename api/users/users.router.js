6db62a8dfdc4607a47ef222b531621d965e71711
Birame-bi <76884201+Birame-bi@users.noreply.github.com>
2024-05-03 17:45:29 +0200

    Update users.router.js

--- api/users/users.router.js
+++ api/users/users.router.js
@@ -2,6 +2,10 @@ const express = require("express");
 const usersController = require("./users.controller");
 const router = express.Router();
 
+// Route pour récupérer les données de l'utilisateur actuellement authentifié
+router.get("/me", usersController.me);
+
+// Routes existantes
 router.get("/", usersController.getAll);
 router.get("/:id", usersController.getById);
 router.post("/", usersController.create);
@@ -9,3 +13,4 @@ router.put("/:id", usersController.update);
 router.delete("/:id", usersController.delete);
 
 module.exports = router;
+
